68917cbbaf117cb4e9007e46ea4266fb2864272d
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-16 22:18:06 -0400

    Use np.random

--- cvi620-project/data_augmentation.py
+++ cvi620-project/data_augmentation.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import random
 
 
 def augment_data(images, steerings):
@@ -15,7 +14,7 @@ def augment_data(images, steerings):
         aug_steerings.append(steering)
 
         # Augment 20% of the dataset
-        if random.randint(1, 5) == 1:
+        if np.random.randint(1, 5) == 1:
             h, w = img.shape[:2]
 
             # Flip horizontally
@@ -27,14 +26,14 @@ def augment_data(images, steerings):
             bright = cv2.cvtColor(bright, cv2.COLOR_YUV2BGR)
             hsv = cv2.cvtColor(bright, cv2.COLOR_BGR2HSV)
             hsv = hsv.astype(np.float32)
-            hsv[:, :, 2] *= random.uniform(0.5, 1.5)
+            hsv[:, :, 2] *= np.random.uniform(0.5, 1.5)
             hsv = np.clip(hsv, 0, 255).astype(np.uint8)
             bright = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
             aug = cv2.cvtColor(bright, cv2.COLOR_BGR2YUV)
             aug = aug.astype(np.float32) / 255.0
 
             # Zoom
-            zoom_factor = random.uniform(1.0, 1.2)  # only zoom in
+            zoom_factor = np.random.uniform(1.0, 1.2)  # only zoom in
             zh, zw = int(h * zoom_factor), int(w * zoom_factor)
             aug = cv2.resize(aug, (zw, zh))
             start_y = (zh - h) // 2
@@ -42,14 +41,14 @@ def augment_data(images, steerings):
             aug = aug[start_y : start_y + h, start_x : start_x + w]
 
             # Shift
-            dx = random.randint(-30, 30)
-            dy = random.randint(-10, 10)
+            dx = np.random.randint(-30, 30)
+            dy = np.random.randint(-10, 10)
             M = np.float32([[1, 0, dx], [0, 1, dy]])
             aug = cv2.warpAffine(aug, M, (w, h))
             aug_steering += dx * 0.003
 
             # Rotate
-            angle = random.uniform(-10, 10)
+            angle = np.random.uniform(-10, 10)
             M = cv2.getRotationMatrix2D((w // 2, h // 2), angle, 1)
             aug = cv2.warpAffine(aug, M, (w, h))
             aug_steering += angle * 0.01
